c28d6eec0753e51a9e9dfb91741a076057329526
Andriy Oblivantsev <eslider@gmail.com>
2018-05-19 16:21:10 +0100

    Improve NodeManager(Iteratable) and NodeService with getName method

--- src/Eslider/NodeManager.php
+++ src/Eslider/NodeManager.php
@@ -2,13 +2,18 @@
 
 namespace Eslider;
 
-class NodeManager
+class NodeManager implements \Iterator
 {
     /**
      * @var array NodeService
      */
     protected $list = [];
 
+    /**
+     * NodeManager constructor.
+     *
+     * @param array $nodeInfos
+     */
     public function __construct(array $nodeInfos)
     {
         foreach ($nodeInfos as $nodeInfo) {
@@ -17,15 +22,65 @@ class NodeManager
         }
     }
 
-    public function getStatus(): array
+    /**
+     * Return the current element
+     *
+     * @link  http://php.net/manual/en/iterator.current.php
+     * @return NodeService
+     * @since 5.0.0
+     */
+    public function current()
     {
-        $r = [];
-        foreach ($this->list as $node) {
-            $status         = $node->getStatus();
-            $url            = parse_url($node->apiUrl);
-            $status['name'] = $url['host'];
-            $r[]            = $status;
-        }
-        return $r;
+        return current($this->list);
+    }
+
+    /**
+     * Move forward to next element
+     *
+     * @link  http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function next()
+    {
+        next($this->list);
+    }
+
+    /**
+     * Return the key of the current element
+     *
+     * @link  http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     * @since 5.0.0
+     */
+    public function key()
+    {
+        return key($this->list);
+    }
+
+    /**
+     * Checks if current position is valid
+     *
+     * @link  http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     * @since 5.0.0
+     */
+    public function valid()
+    {
+        $nodeService = $this->current();
+        return $nodeService !== false && $nodeService !== null;
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     *
+     * @link  http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function rewind()
+    {
+        reset($this->list);
     }
 }
--- src/Eslider/NodeService.php
+++ src/Eslider/NodeService.php
@@ -4,9 +4,14 @@ namespace Eslider;
 
 class NodeService
 {
-
+    /** @var string API Node URL */
     public $apiUrl;
 
+    /**
+     * NodeService constructor.
+     *
+     * @param $args
+     */
     public function __construct($args)
     {
         foreach ($args as $key => $value) {
@@ -14,19 +19,42 @@ class NodeService
         }
     }
 
-
     /**
+     * Query node
+     *
      * @param string $args
-     * @return string
+     * @param bool   $json Convert JSON result to Array?
+     * @return string|array
      */
-    public function query($args)
+    public function query($args, $json = true)
     {
-        $url = $this->apiUrl . rawurlencode($args);
-        return file_get_contents($url);
+        $url      = $this->apiUrl . rawurlencode($args);
+        $contents = file_get_contents($url);
+        if ($json) {
+            $contents = json_decode($contents, true);
+        }
+        return $contents;
     }
 
+    /**
+     * Get node status info
+     *
+     * @return array|string
+     */
     public function getStatus()
     {
-        return json_decode($this->query('mnsync status'), true);
+        return $this->query('mnsync status');
+    }
+
+    /**
+     * Get node name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        $url            = parse_url($this->apiUrl);
+        $status['name'] = $url['host'];
+        return $url['host'];
     }
 }
